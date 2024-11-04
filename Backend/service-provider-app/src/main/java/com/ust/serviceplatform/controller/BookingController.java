dec0d3bc101dd665b76b88667d4bfb0d16048ac7
johnwesley18 <doddamanijohn123@gmail.com>
2024-11-04 15:50:46 +0530

    Added the API_Gateway

--- Backend/service-provider-app/src/main/java/com/ust/serviceplatform/controller/BookingController.java
+++ Backend/service-provider-app/src/main/java/com/ust/serviceplatform/controller/BookingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/bookings")
+@RequestMapping("/api/bookings")
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
